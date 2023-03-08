945c2abec391d6db2b4206abda20f0cb8a3449fc
cryndry <ismailkaraagac94@gmail.com>
2023-03-08 17:40:21 +0300

    Template updated

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import s from "./Footer.module.css";
+
+const Footer = () => {
+    return (
+        <footer className={s.Footer}>Ismail * &copy; 2023</footer>
+    );
+};
+
+export default Footer;
